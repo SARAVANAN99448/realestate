fc446e36788cee008faf3280408088a119b69167
SARAVANAN99448 <saravanasaro0000@gmail.com>
2025-03-07 22:38:19 +0530

    realestate

--- realestate/src/main.jsx
+++ realestate/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import Contact from './components/contact.jsx'
+import Contact from './components/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
